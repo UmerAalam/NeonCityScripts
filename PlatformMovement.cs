766c97505d42f6a456e3815ca19c2e8cdd89120b
UmerAalam <umerrazzaq.dev@gmail.com>
2022-08-20 12:10:24 +0500

    Update PlatformMovement.cs

--- PlatformMovement.cs
+++ PlatformMovement.cs
@@ -7,13 +7,20 @@ public class PlatformMovement : MonoBehaviour
     [SerializeField] float moveTime;
     [SerializeField] Vector3 endPos;
     Vector3 startPos;
+    bool isPlayer;
     private void Start()
     {
+        isPlayer = false;
         startPos = transform.position;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
+            isPlayer = true;
+    }
+    private void Update()
+    {
+        if (isPlayer)
             MovePlatform();
     }
     void MovePlatform()
