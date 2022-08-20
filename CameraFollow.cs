019a75d74316c8d111a1e7da2eb8fbb082b0cf66
UmerAalam <umerrazzaq.dev@gmail.com>
2022-08-20 09:15:04 +0500

    CameraFollow

---
+++ CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float cameraLerp;
+    [SerializeField] Vector3 offset;
+
+    private void LateUpdate()
+    {
+        CameraX();
+    }
+    void CameraX()
+    {
+        transform.position = Vector2.Lerp(transform.position + offset,player.transform.position, cameraLerp * Time.deltaTime);
+    }
+
+}
